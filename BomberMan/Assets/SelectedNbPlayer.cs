a8c9926420000f41511357d9cae3aa6e85d1d6f8
Delanium <axel.meyer@epitech.eu>
2016-12-11 17:27:49 +0900

    Add choose number players

---
+++ BomberMan/Assets/SelectedNbPlayer.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SelectedNbPlayer : MonoBehaviour {
+
+    private int nb_players;
+
+    public Text text;
+
+    void Start()
+    {
+        nb_players = 2;
+        text.text = "Players : " + nb_players;
+    }
+
+    public void IncreaseNbPlayers()
+    {
+        if (nb_players < 4)
+        {
+            nb_players++;
+            text.text = "Players : " + nb_players;
+        }
+    }
+
+    public void DecreaseNbPlayers()
+    {
+        if (nb_players > 2)
+        {
+            nb_players--;
+            text.text = "Players : " + nb_players;
+        }
+    }
+
+    public int GetNbPlayers()
+    {
+        return nb_players;
+    }
+}
